82cc68f765d38d367c971f0ddb57869cbe591182
BogdanBalanar <badikradik123@gmail.com>
2021-03-11 13:37:14 +0300

    feat: add solutions

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,28 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain : [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!Number.isInteger(position) || this.chain[position - 1] === undefined) {
+      this.chain.length = 0;
+      throw new Error('err');
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const res = '( ' + this.chain.map((element) => '' + element).join(' )~~( ') + ' )';
+    this.chain.length = 0;
+    return res;
   }
 };
-
 module.exports = chainMaker;
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,50 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modification = true) {
+    this.direct = modification;
+  }
+  encrypt(input, key) {
+    if (input === undefined || key === undefined) {
+      throw new Error();
+    }
+    let iNPUT = input.toUpperCase().split('');
+    let kEY = key.toUpperCase();
+	  let spaces = 0;
+    for (let i = 0; i < iNPUT.length; i++) {
+      let charCode = iNPUT[i].codePointAt(0);
+      if (charCode >= 65 && charCode < 91) {
+        let index = kEY.codePointAt((i - spaces) % key.length) - 65;
+        let search = charCode + index;
+        if (search > 90) {
+          search -= 26;
+        }
+        iNPUT[i] = String.fromCodePoint(search);
+      } else {
+        spaces++;
+      }
+    }
+	  return this.direct ? iNPUT.join('') : iNPUT.reverse().join('');
+  }
+  decrypt(reverseInput, key) {
+    if (reverseInput === undefined || key === undefined) {
+      throw new Error();
+    }
+    let iNPUT = reverseInput.toUpperCase().split('');
+    let kEY = key.toUpperCase();
+	  let spaces = 0;
+    for (let i = 0; i < iNPUT.length; i++) {
+      let charCode = iNPUT[i].codePointAt(0);
+      if (charCode >= 65 && charCode < 91) {
+        let index = kEY.codePointAt((i - spaces) % key.length) - 65;
+        let search = charCode - index;
+        if (search < 65) {
+          search += 26;
+        }
+        iNPUT[i] = String.fromCodePoint(search);
+      } else {
+        spaces++;
+      }
+    }
+	  return this.direct ? iNPUT.join('') : iNPUT.reverse().join('');
   }
 }
-
 module.exports = VigenereCipheringMachine;
